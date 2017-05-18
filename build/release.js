0169f0d37a544c09e84a3beeb1ceffe8ea3efb9f
Minocoko <6612504@qq.com>
2017-05-18 21:29:34 +0800

    update version

--- build/release.js
+++ build/release.js
@@ -1,11 +1,11 @@
 const semver = require('semver')
 const fs = require('fs')
 const path = require('path')
-const shell = require('shellJS')
+const shell = require('shelljs')
 
 if (!shell.which('git')) {
-  shell.echo('Sorry, this script requires git');
-  shell.exit(1);
+  shell.echo('Sorry, this script requires git')
+  shell.exit(1)
 } else if (process.argv.length >= 3) {
   const ver = process.argv[2]
   if (semver.valid(ver) === ver) {
@@ -13,12 +13,15 @@ if (!shell.which('git')) {
     let pkg = require('../package.json')
     pkg.version = ver
     fs.writeFileSync(path.join(__dirname, '../package.json'), JSON.stringify(pkg, null, '\t'), 'utf-8')
+    console.log('update version')
+
+    // add release tag
+    shell.exec(`git tag -a v${ver} -m "release v${ver}" && git push origin v${ver}`)
+    console.log('add release tag')
 
     // push to git
-    if (shell.exec('git add ./ && git commit -m "update version" && git push origin').code !== 0) {
-      shell.echo('Error: Git commit failed')
-      shell.exit(1)
-    }
+    shell.exec('git add ./ && git commit -m "update version" && git push origin')
+    console.log('git push')
   }
 } else {
   console.error('version required')
