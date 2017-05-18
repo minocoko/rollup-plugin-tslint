d0b41324e72a219cb76b07b38e05d897842fb7de
Minocoko <6612504@qq.com>
2017-05-18 21:16:16 +0800

    update version

--- build/release.js
+++ build/release.js
@@ -1,15 +1,24 @@
 const semver = require('semver')
 const fs = require('fs')
 const path = require('path')
+const shell = require('shellJS')
 
-if (process.argv.length >= 3) {
+if (!shell.which('git')) {
+  shell.echo('Sorry, this script requires git');
+  shell.exit(1);
+} else if (process.argv.length >= 3) {
   const ver = process.argv[2]
   if (semver.valid(ver) === ver) {
+    // update version
     let pkg = require('../package.json')
     pkg.version = ver
-
     fs.writeFileSync(path.join(__dirname, '../package.json'), JSON.stringify(pkg, null, '\t'), 'utf-8')
-    console.log('update version')
+
+    // push to git
+    if (shell.exec('git add ./ && git commit -m "update version" && git push origin').code !== 0) {
+      shell.echo('Error: Git commit failed')
+      shell.exit(1)
+    }
   }
 } else {
   console.error('version required')
