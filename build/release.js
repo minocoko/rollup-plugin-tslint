bbc92586d5d09afa73c567f46e489f094d92a86b
Minocoko <6612504@qq.com>
2017-05-22 10:08:45 +0800

    update

--- build/release.js
+++ build/release.js
@@ -9,19 +9,27 @@ if (!shell.which('git')) {
 } else if (process.argv.length >= 3) {
   const ver = process.argv[2]
   if (semver.valid(ver) === ver) {
+    // switch to master && merge
+    shell.exec('git checkout master && git merge dev')
+    console.log('git push')
+
     // update version
     let pkg = require('../package.json')
     pkg.version = ver
     fs.writeFileSync(path.join(__dirname, '../package.json'), JSON.stringify(pkg, null, '\t'), 'utf-8')
     console.log('update version')
 
+    // push to git
+    shell.exec('git add ./ && git commit -m "update version" && git push origin')
+    console.log('git push')
+
     // add release tag
     shell.exec(`git tag -a v${ver} -m "release v${ver}" && git push origin v${ver}`)
     console.log('add release tag')
 
-    // push to git
-    shell.exec('git add ./ && git commit -m "update version" && git push origin')
-    console.log('git push')
+    shell.exec('git checkout dev && git rebase master && git push dev')
+
+    shell.exec('npm publish')
   }
 } else {
   console.error('version required')
